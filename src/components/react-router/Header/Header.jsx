68979e4a50814f7fc058724651e475ceb6be2d6c
Hitesh ahire <hbahire2014@gmail.com>
2025-10-05 20:51:43 +0530

    file modification

--- src/components/react-router/Header/Header.jsx
+++ src/components/react-router/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom'
 
+// dummy header component
 export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
